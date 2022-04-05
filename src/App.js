5a16b812a841c5385320fbeef5ad312a1e793bc0
shina1 <oswasiu@gmail.com>
2022-04-05 17:37:23 +0100

    modified

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import Cart from './screens/cart'
 // import Login from './screens/login'
 // import Register from './screens/register'
 // import SingleProduct from './screens/single-product'
-// import Home from './screens/home-screen'
+import Home from './screens/home-screen'
 // import ProductList from './screens/product-list/index.js'
 
 const App = () => {
   return (
     <div>
-      {/* <Home /> */}
+      <Home />
       {/* <ProductList /> */}
       {/* <SingleProduct /> */}
       {/* <Register /> */}
       {/* <Login /> */}
-      <Cart /> 
+      {/* <Cart />  */}
     </div>
   )
 }
