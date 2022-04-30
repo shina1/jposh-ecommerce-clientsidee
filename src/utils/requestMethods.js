9fb94df5d300b8926a7f9f04f7caddcd76c30b6d
shina1 <oswasiu@gmail.com>
2022-04-30 21:07:04 +0100

    fixed responsive single product page

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -3,8 +3,6 @@ import axios from "axios"
 // export const PRODUCTION_BASE_URL = "http://localhost:5500/api/v1/"
 export const PRODUCTION_BASE_URL = "https://ancient-beach-60604.herokuapp.com/api/v1/"
 
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyNDg4YjAxZWI4Y2RkNmZhMTFlMzUwYyIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY0OTQ4Nzk0MywiZXhwIjoxNjUyMDc5OTQzfQ.mbYfb0irjA95yozWzzVRJUYG1aItCy0yzqRZEB7Ck9g"
-
 // export const publicRequest = axios.create({
 //     baseURL: PRODUCTION_BASE_URL,
 // });
