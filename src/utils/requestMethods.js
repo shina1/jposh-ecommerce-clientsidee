b400740dc21f3b3f1281cc6437faba19ddb90de7
shina1 <oswasiu@gmail.com>
2022-05-05 18:03:38 +0100

    change connection request URL

--- src/utils/requestMethods.js
+++ src/utils/requestMethods.js
@@ -1,7 +1,10 @@
 import axios from "axios"
 
-export const PRODUCTION_BASE_URL = "http://localhost:5500/api/v1/"
-// export const PRODUCTION_BASE_URL = "https://ancient-beach-60604.herokuapp.com/api/v1/"
+
+// export const PRODUCTION_BASE_URL = "http://141.136.42.247/api/v1"
+
+// export const PRODUCTION_BASE_URL = "http://localhost:5500/api/v1/"
+export const PRODUCTION_BASE_URL = "https://ancient-beach-60604.herokuapp.com/api/v1/"
 
 // export const publicRequest = axios.create({
 //     baseURL: PRODUCTION_BASE_URL,
